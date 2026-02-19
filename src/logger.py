7cc35aca8d7d7f507da05835414a4f023b98b3bf
Abhishek pandey <abhishekpandey0478@gmail.com>
2026-02-19 14:45:46 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,14 +3,21 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# 🔥 REMOVE ALL EXISTING HANDLERS (CRITICAL)
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    level=logging.INFO,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO,
-
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH, mode="a"),
+        logging.StreamHandler()
+    ]
 )
