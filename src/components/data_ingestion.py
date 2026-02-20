510728205b6aaa13cf516b9ce0b7afdc5c207743
Abhishek pandey <abhishekpandey0478@gmail.com>
2026-02-20 13:58:50 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,17 +1,17 @@
 import os
 import sys
-
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
 
-
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-@dataclass                                                                  
+
+@dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
@@ -30,7 +30,6 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            logging.info("STEP 3: artifacts directory created")
 
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -51,12 +50,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-
-
-    
-
-
-
-
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
